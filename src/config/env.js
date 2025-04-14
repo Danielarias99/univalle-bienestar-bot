bedd453c324656e9510db957a29d8342ef06f1d8
Danielarias99 <daniel.ariferia99@gmail.com>
2025-04-13 23:28:51 -0500

    fix: agregar GOOGLE_CREDENTIALS_BASE64 a la exportación de config

--- src/config/env.js
+++ src/config/env.js
@@ -7,5 +7,7 @@ export default {
   API_TOKEN: process.env.API_TOKEN,
   BUSINESS_PHONE: process.env.BUSINESS_PHONE,
   API_VERSION: process.env.API_VERSION,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  GOOGLE_CREDENTIALS_BASE64: process.env.GOOGLE_CREDENTIALS_BASE64,
   PORT: process.env.PORT || 3000,
 };
